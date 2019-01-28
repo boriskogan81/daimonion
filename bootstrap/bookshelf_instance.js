5cae5fba654f2a4f9c5e3a95b2e0daf6fad77a02
Baruch Kogan <baruch.kogan@silverbolt.ai>
2019-01-28 16:28:37 +0200

    taught app to properly use GCloud SQL

--- bootstrap/bookshelf_instance.js
+++ bootstrap/bookshelf_instance.js
@@ -8,6 +8,12 @@ if (process.env.NODE_ENV === 'test') {
 }
 else{
     dbConfig = require('../config/db_config')['production'];
+    dbConfig.user = process.env.SQL_USER;
+    dbConfig.database = process.env.SQL_DATABASE;
+    dbConfig.password = process.env.SQL_PASSWORD;
+    if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+        dbConfig.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+    }
     knex = require('knex')(dbConfig);
 }
 
